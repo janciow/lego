9ae5ea8c680495aac106e5ac9127663af4d76f3a
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2020-12-16 20:45:03 +0100

    query builder

--- server/controllers/brick-balance-controllers.js
+++ server/controllers/brick-balance-controllers.js
@@ -1,4 +1,5 @@
 const connection = require("../../db/db_connect");
+const bbQueryBuilder = require("./query-utils/brick-balance");
 
 const getBrickBalanceBySet = (req, res) => {
   const setNumber = req.params.setNumber;
@@ -42,112 +43,24 @@ const legoPiratesShipBrickList = (req, res) => {
   ];
 
   const q = `
-    SELECT img_pathname,
-	price,
-	model_id,
-	element_id,
-	quantity_total,
-	s6286.quantity as set_6286_q,
-	s6285.quantity as set_6285_q,
-	s10210.quantity as set_10210_q,
-	s4195.quantity as set_4195_q,
-	s6243.quantity as set_6243_q,
-	s6274.quantity as set_6274_q,
-	s70413.quantity as set_70413_q,
-	(
-		ifnull(s6286.quantity, 0) + ifnull(s6285.quantity, 0) + ifnull(s10210.quantity, 0) + ifnull(s4195.quantity, 0) + ifnull(s6243.quantity, 0) + ifnull(s6274.quantity, 0) + ifnull(s70413.quantity, 0)
-	) as total_q,
-	s6286.lego_set_id,
-	s6285.lego_set_id,
-	s10210.lego_set_id,
-	s4195.lego_set_id,
-	s6243.lego_set_id,
-	s6274.lego_set_id,
-	s70413.lego_set_id,
-	brick.description
-FROM brick
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "6286"
-		ORDER BY brick.description
-	) as s6286 ON s6286.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "6285"
-		ORDER BY brick.description
-	) as s6285 ON s6285.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "10210"
-		ORDER BY brick.description
-	) as s10210 ON s10210.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "4195"
-		ORDER BY brick.description
-	) as s4195 ON s4195.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "6243"
-		ORDER BY brick.description
-	) as s6243 ON s6243.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "6274"
-		ORDER BY brick.description
-	) as s6274 ON s6274.brick_id = brick.element_id
-	LEFT JOIN (
-		SELECT brick.element_id as brick_id,
-			lego_set_parts.quantity as quantity,
-			lego_set_parts.lego_set_id as lego_set_id
-		FROM lego_set_parts
-			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-		WHERE lego_set_parts.lego_set_id = "70413"
-		ORDER BY brick.description
-	) as s70413 ON s70413.brick_id = brick.element_id
-WHERE (
-		s6286.quantity IS NOT NULL
-		or s6285.quantity IS NOT NULL
-		or s10210.quantity IS NOT NULL
-		or s4195.quantity IS NOT NULL
-		or s6243.quantity IS NOT NULL
-		or s6274.quantity IS NOT NULL
-		or s70413.quantity IS NOT NULL
-	)
-	and (
-		s6286.lego_set_id = ?
-		or s6285.lego_set_id = ?
-		or s10210.lego_set_id = ?
-		or s4195.lego_set_id = ?
-		or s6243.lego_set_id = ?
-		or s6274.lego_set_id = ?
-		or s70413.lego_set_id = ?
-	)
-ORDER BY brick.description;
+	SELECT img_pathname,
+		price,
+		model_id,
+		element_id,
+		quantity_total,
+		${bbQueryBuilder.bbSelect(['6286','6285','10210', '4195','6243','6274','70413'])}
+		brick.description
+	FROM brick
+		${bbQueryBuilder.bbLeftJoin('6286')}
+		${bbQueryBuilder.bbLeftJoin('6285')}
+		${bbQueryBuilder.bbLeftJoin('10210')}
+		${bbQueryBuilder.bbLeftJoin('4195')}
+		${bbQueryBuilder.bbLeftJoin('6243')}
+		${bbQueryBuilder.bbLeftJoin('6274')}
+		${bbQueryBuilder.bbLeftJoin('70413')}
+	WHERE 
+		${bbQueryBuilder.bbWhere(['6286','6285','10210', '4195','6243','6274','70413'])}
+	ORDER BY brick.description;
     `;
 
   connection.query(q, qParams, function (err, results) {
@@ -163,62 +76,20 @@ const legoSWCloneShipBrickList = (req, res) => {
   const qParams = [setNumber, setNumber, setNumber];
 
   const q = `
-  SELECT img_pathname,
-  price,
-  model_id,
-  element_id,
-  quantity_total,
-  s7675.quantity as set_7675_q,
-  s7676.quantity as set_7676_q,
-  s10195.quantity as set_10195_q,
-
-  (
-	  ifnull(s7675.quantity, 0) + ifnull(s7676.quantity, 0) + ifnull(s10195.quantity, 0) 
-  ) as total_q,
-  s7675.lego_set_id,
-  s7676.lego_set_id,
-  s10195.lego_set_id,
-  brick.description
-FROM brick
-  LEFT JOIN (
-	  SELECT brick.element_id as brick_id,
-		  lego_set_parts.quantity as quantity,
-		  lego_set_parts.lego_set_id as lego_set_id
-	  FROM lego_set_parts
-		  LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-	  WHERE lego_set_parts.lego_set_id = "7675"
-	  ORDER BY brick.description
-  ) as s7675 ON s7675.brick_id = brick.element_id
-  LEFT JOIN (
-	  SELECT brick.element_id as brick_id,
-		  lego_set_parts.quantity as quantity,
-		  lego_set_parts.lego_set_id as lego_set_id
-	  FROM lego_set_parts
-		  LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-	  WHERE lego_set_parts.lego_set_id = "7676"
-	  ORDER BY brick.description
-  ) as s7676 ON s7676.brick_id = brick.element_id
-	  LEFT JOIN (
-	  SELECT brick.element_id as brick_id,
-		  lego_set_parts.quantity as quantity,
-		  lego_set_parts.lego_set_id as lego_set_id
-	  FROM lego_set_parts
-		  LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
-	  WHERE lego_set_parts.lego_set_id = "10195"
-	  ORDER BY brick.description
-  ) as s10195 ON s10195.brick_id = brick.element_id
-WHERE (
-	  s7675.quantity IS NOT NULL
-	  or s7676.quantity IS NOT NULL
-	  or s10195.quantity IS NOT NULL
-  )
-  and (
-	  s7675.lego_set_id = ?
-	  or s7676.lego_set_id = ?
-	  or s10195.lego_set_id = ?
-
-  )
-ORDER BY brick.description;
+	SELECT img_pathname,
+		price,
+		model_id,
+		element_id,
+		quantity_total,
+		${bbQueryBuilder.bbSelect(['7675','7676','10195'])}
+		brick.description
+	FROM brick
+		${bbQueryBuilder.bbLeftJoin('7675')}
+		${bbQueryBuilder.bbLeftJoin('7676')}
+		${bbQueryBuilder.bbLeftJoin('10195')}
+	WHERE
+		${bbQueryBuilder.bbWhere(['7675','7676','10195'])}
+	ORDER BY brick.description;
     `;
 
   connection.query(q, qParams, function (err, results) {
---
+++ server/controllers/query-utils/brick-balance.js
@@ -0,0 +1,80 @@
+const bbLeftJoin = (legoSetNumber) => {
+    return `
+	LEFT JOIN (
+		SELECT brick.element_id as brick_id,
+			lego_set_parts.quantity as quantity,
+			lego_set_parts.lego_set_id as lego_set_id
+		FROM lego_set_parts
+			LEFT JOIN brick ON lego_set_parts.brick_id = brick.element_id
+		WHERE lego_set_parts.lego_set_id = "${legoSetNumber}"
+		ORDER BY brick.description
+	) as s${legoSetNumber} ON s${legoSetNumber}.brick_id = brick.element_id
+    `
+}
+
+// example
+// (
+//     s7675.quantity IS NOT NULL
+//     or s7676.quantity IS NOT NULL
+//     or s10195.quantity IS NOT NULL
+// )
+// and (
+//     s7675.lego_set_id = ?
+//     or s7676.lego_set_id = ?
+//     or s10195.lego_set_id = ?
+// )
+
+const bbWhere = (legoSetNumbers) => {
+    return `
+      (
+        ${legoSetNumbers.map((setNum, index) => {
+        return index === 0 ? `s${setNum}.quantity IS NOT NULL` : `or s${setNum}.quantity IS NOT NULL`
+    }).join(' ')
+        }
+      ) and (          
+        ${legoSetNumbers.map((setNum, index) => {
+            return index === 0 ? `s${setNum}.lego_set_id = ?` : `or s${setNum}.lego_set_id = ?`
+        }).join(' ')
+        }
+      )
+    `;
+};
+
+// example
+// s7675.quantity as set_7675_q,
+// s7676.quantity as set_7676_q,
+// s10195.quantity as set_10195_q,
+// (
+//     ifnull(s7675.quantity, 0) + ifnull(s7676.quantity, 0) + ifnull(s10195.quantity, 0) 
+// ) as total_q,
+// s7675.lego_set_id,
+// s7676.lego_set_id,
+// s10195.lego_set_id,
+
+const bbSelect = (legoSetNumbers) => {
+    return `
+      
+        ${legoSetNumbers.map((setNum) => {
+        return `s${setNum}.quantity as set_${setNum}_q,`
+    }).join(' ')
+        }
+
+        (
+
+            ${legoSetNumbers.map((setNum, index) => {
+            return index === 0 ? `ifnull(s${setNum}.quantity, 0)` : `+ ifnull(s${setNum}.quantity, 0)`
+        }).join(' ')
+
+        }
+        ) as total_q,
+     
+        ${legoSetNumbers.map((setNum) => {
+            return `s${setNum}.lego_set_id,`
+        }).join(' ')
+        }
+    `;
+};
+
+exports.bbLeftJoin = bbLeftJoin;
+exports.bbWhere = bbWhere; 
+exports.bbSelect = bbSelect; 
