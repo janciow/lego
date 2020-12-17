6fdff927fd4dcdda045f6c26d8c7528db0076d0a
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2020-12-17 02:17:56 +0100

    last element

--- insertData/updateBrickQuantity.js
+++ insertData/updateBrickQuantity.js
@@ -5,7 +5,7 @@ const connection = require('../db/db_connect');
 
 let brickData = JSON.parse(fs.readFileSync(`jsons_lego/db_tables_json/brick_table.json`));
 
-brickData.forEach(element => {
+brickData.forEach((element, index) => {
     brickQuery = `UPDATE brick SET quantity_total = ${element.quantity_total} WHERE element_id = '${element.element_id}'`;
 
     connection.query(brickQuery, function (error, result) {
@@ -13,9 +13,10 @@ brickData.forEach(element => {
             console.log('this.sql', this.sql); //command/query
             console.log(error);
         }
+        if (index >= brickData.length -1 )
+            console.log('done last=  ', index);
     });
 });
-
 
 connection.end();
 
