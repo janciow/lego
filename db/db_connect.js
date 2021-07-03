84df4a3a50090e0cfdcdcd7f835a17728f85d2b7
jan Bancarewiz <jan@bancarewicz@gmail.com>
2021-07-03 18:42:31 +0200

    env update

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 
-const MY_APP_SECRET = process.env.APP_SECRET;
-const MY_APP_USER= process.env.APP_USER;
-const MY_APP_DB_NAME= process.env.APP_DB_NAME;
+const APP_SECRET = process.env.APP_SECRET;
+const APP_USER= process.env.APP_USER;
+const APP_DB_NAME= process.env.APP_DB_NAME;
+const APP_HOST_URL= process.env.APP_HOST_URL;
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: MY_APP_USER,
-    password: MY_APP_SECRET,
-    database: MY_APP_DB_NAME,
+    host: APP_HOST_URL,
+    user: APP_USER,
+    password: APP_SECRET,
+    database: APP_DB_NAME,
     multipleStatements: true
 });
 
